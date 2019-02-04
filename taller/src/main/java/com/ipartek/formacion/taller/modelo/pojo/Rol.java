3b3e5ec8c623e612117fd7bf9f13421947f081ec
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-02-04 12:20:08 +0100

    servicio rest persona funcionando

---
+++ taller/src/main/java/com/ipartek/formacion/taller/modelo/pojo/Rol.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.taller.modelo.pojo;
+
+public class Rol {
+	private int id;
+	private String nombre;
+
+	public Rol() {
+		super();
+		this.id = -1;
+		this.nombre = "";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Rol(int id, String nombre) {
+		this();
+		this.setId(id);
+		this.setNombre(nombre);
+	}
+
+	@Override
+	public String toString() {
+		return "Rol [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+}
